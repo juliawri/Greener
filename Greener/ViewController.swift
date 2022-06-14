576e2217477d2cf07db1fb929b6d744707af24e8
Julia Wright <jwrighthfx@gmail.com>
2022-06-14 15:01:55 -0300

    Initial Commit

---
+++ Greener/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Greener
+//
+//  Created by Julia Wright on 2022-06-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
