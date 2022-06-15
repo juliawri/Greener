9078c28bb4ad7dcd8278fc4ddfc79742247add71
Julia Wright <jwrighthfx@gmail.com>
2022-06-15 15:03:03 -0300

    fixed spacing issues with first score button/label

--- Greener/ViewController.swift
+++ Greener/ViewController.swift
@@ -45,38 +45,38 @@ class ViewController: UIViewController {
         titleLabel.layer.cornerRadius =  titleLabel.frame.width/2
         titleLabel.layer.masksToBounds = true
 
-        //first score label
-                scoreLabel1.frame = CGRect(x: (self.view.frame.width / 2) - 120, y: 300, width: 125, height: 75)
-                scoreLabel1.text = "recycle 5 times"
-                self.view.addSubview(scoreLabel1)
-                //background color
-                scoreLabel1.backgroundColor = UIColorFromHex(rgbValue: 0x508A43, alpha: 1)
-                //text alignment
-                scoreLabel1.textAlignment = .center
-                //text colour
-                scoreLabel1.textColor = UIColorFromHex(rgbValue: 0xDEE4DD, alpha: 1)
-                //changing font
-                scoreLabel1.font = UIFont(name: "Avenir Next", size: 18)
-                //rounding corners
-                scoreLabel1.layer.cornerRadius =  15
-                scoreLabel1.layer.masksToBounds = true
+//first score label
+        scoreLabel1.frame = CGRect(x: (self.view.frame.width / 2) - 117.5, y: 300, width: 200, height: 75)
+        scoreLabel1.text = "recycle 5 times"
+        self.view.addSubview(scoreLabel1)
+        //background color
+        scoreLabel1.backgroundColor = UIColorFromHex(rgbValue: 0xFF0000, alpha: 1)
+        //text alignment
+        scoreLabel1.textAlignment = .center
+        //text colour
+        scoreLabel1.textColor = UIColorFromHex(rgbValue: 0x000000, alpha: 1)
+        //changing font
+        scoreLabel1.font = UIFont(name: "Avenir Next", size: 18)
+        //rounding corners
+        scoreLabel1.layer.cornerRadius =  15
+        scoreLabel1.layer.masksToBounds = true
 
+        
 //first score button with square buttons
         view.addSubview(score1Button2)
         //framing
-        score1Button2.frame = CGRect(x: (self.view.frame.width / 2) - 65, y: 300, width: 65, height: 75)
+        score1Button2.frame = CGRect(x: (self.view.frame.width / 2) + 67.5, y: 300, width: 25, height: 75)
         //background color
         score1Button2.backgroundColor = UIColorFromHex(rgbValue: 0x508A43, alpha: 1)
         //tag is used for differentiating buttons
-        score1Button2.tag = 2
+        score1Button2.tag = 1
 
-        
 //first score button with rounded corners
         view.addSubview(score1Button1)
         //framing
-        score1Button1.frame = CGRect(x: (self.view.frame.width / 2) - 65, y: 300, width: 130, height: 75)
+        score1Button1.frame = CGRect(x: (self.view.frame.width / 2) + 67.5, y: 300, width: 50, height: 75)
         //background color
-        score1Button1.backgroundColor = UIColorFromHex(rgbValue: 0xFF0000, alpha: 1)
+        score1Button1.backgroundColor = UIColorFromHex(rgbValue: 0x508A43, alpha: 1)
         //title
         score1Button1.setTitle("hi", for: .normal)
         //making the button clickable
@@ -92,7 +92,7 @@ class ViewController: UIViewController {
 //function for when first button is tapped
     @objc func buttonAction(sender: UIButton!) {
             if sender.tag == 1 {
-
+                print("button pressed")
             }
         }
 
