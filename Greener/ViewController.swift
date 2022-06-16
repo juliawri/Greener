1842f02fc7fce2a3b2ca671b0a64be3e5db6034a
Julia Wright <jwrighthfx@gmail.com>
2022-06-16 13:19:33 -0300

    Added functionality to button and created a new button

--- Greener/ViewController.swift
+++ Greener/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
     var score1Button2 = UIButton()
     var scoreLabel1 = UILabel()
     
+    var score2Button1 = UIButton()
+    var score2Button2 = UIButton()
+    var scoreLabel2 = UILabel()
+    
     //array of images
     var images = [UIImage(named: "Circle_1")!, UIImage(named: "Circle_2")!]
     
@@ -34,8 +38,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
 // circular title label at top of page
-        titleLabel.frame = CGRect(x: (self.view.frame.width / 2) - 62.5, y: 50, width: 125, height: 125)
-        titleLabel.text = "#\n Points"
+        titleLabel.frame = CGRect(x: (self.view.frame.width / 2) - 100, y: 150, width: 200, height: 200)
+        titleLabel.text = "\(points)\n Points"
         self.view.addSubview(titleLabel)
         // number of lines
         titleLabel.numberOfLines = 2
@@ -52,15 +56,15 @@ class ViewController: UIViewController {
         titleLabel.layer.masksToBounds = true
 
 //first score label
-        scoreLabel1.frame = CGRect(x: (self.view.frame.width / 2) - 117.5, y: 300, width: 200, height: 75)
+        scoreLabel1.frame = CGRect(x: (self.view.frame.width / 2) - 117.5, y: 400, width: 200, height: 75)
         scoreLabel1.text = "recycle 5 times"
         self.view.addSubview(scoreLabel1)
         //background color
-        scoreLabel1.backgroundColor = UIColorFromHex(rgbValue: 0xFF0000, alpha: 1)
+        scoreLabel1.backgroundColor = UIColorFromHex(rgbValue: 0x10440C, alpha: 1)
         //text alignment
         scoreLabel1.textAlignment = .center
         //text colour
-        scoreLabel1.textColor = UIColorFromHex(rgbValue: 0x000000, alpha: 1)
+        scoreLabel1.textColor = UIColorFromHex(rgbValue: 0xDDE4DD, alpha: 1)
         //changing font
         scoreLabel1.font = UIFont(name: "Avenir Next", size: 18)
         //rounding corners
@@ -68,23 +72,23 @@ class ViewController: UIViewController {
         scoreLabel1.layer.masksToBounds = true
 
         
-//first score button with square buttons
+//first score button with square corners
         view.addSubview(score1Button2)
         //framing
-        score1Button2.frame = CGRect(x: (self.view.frame.width / 2) + 67.5, y: 300, width: 25, height: 75)
+        score1Button2.frame = CGRect(x: (self.view.frame.width / 2) + 67.5, y: 400, width: 25, height: 75)
         //background color
-        score1Button2.backgroundColor = UIColorFromHex(rgbValue: 0x508A43, alpha: 1)
+        score1Button2.backgroundColor = UIColorFromHex(rgbValue: 0x4E8D7E, alpha: 1)
         //tag is used for differentiating buttons
         score1Button2.tag = 1
 
 //first score button with rounded corners
         view.addSubview(score1Button1)
         //framing
-        score1Button1.frame = CGRect(x: (self.view.frame.width / 2) + 67.5, y: 300, width: 50, height: 75)
+        score1Button1.frame = CGRect(x: (self.view.frame.width / 2) + 67.5, y: 400, width: 50, height: 75)
         //background color
-        score1Button1.backgroundColor = UIColorFromHex(rgbValue: 0x508A43, alpha: 1)
+        score1Button1.backgroundColor = UIColorFromHex(rgbValue: 0x4E8D7E, alpha: 1)
         //title
-        score1Button1.setTitle("hi", for: .normal)
+        score1Button1.setTitle("+", for: .normal)
         //making the button clickable
         score1Button1.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         //rounding corners
@@ -93,12 +97,66 @@ class ViewController: UIViewController {
         //tag is used for differentiating buttons
         score1Button1.tag = 1
 
+        
+        
+//second score label
+        scoreLabel2.frame = CGRect(x: (self.view.frame.width / 2) - 117.5, y: 500, width: 200, height: 75)
+        scoreLabel2.text = "recycle 5 times"
+                self.view.addSubview(scoreLabel2)
+        //background color
+        scoreLabel2.backgroundColor = UIColorFromHex(rgbValue: 0x10440C, alpha: 1)
+        //text alignment
+        scoreLabel2.textAlignment = .center
+        //text colour
+        scoreLabel2.textColor = UIColorFromHex(rgbValue: 0xDDE4DD, alpha: 1)
+        //changing font
+        scoreLabel2.font = UIFont(name: "Avenir Next", size: 18)
+        //rounding corners
+        scoreLabel2.layer.cornerRadius =  15
+        scoreLabel2.layer.masksToBounds = true
+
+                
+//second score button with square corners
+        view.addSubview(score2Button2)
+        //framing
+        score2Button2.frame = CGRect(x: (self.view.frame.width / 2) + 67.5, y: 500, width: 25, height: 75)
+        //background color
+        score2Button2.backgroundColor = UIColorFromHex(rgbValue: 0x4E8D7E, alpha: 1)
+        //tag is used for differentiating buttons
+        score2Button2.tag = 2
+
+//second score button with rounded corners
+        view.addSubview(score2Button1)
+        //framing
+        score2Button1.frame = CGRect(x: (self.view.frame.width / 2) + 67.5, y: 500, width: 50, height: 75)
+        //background color
+        score2Button1.backgroundColor = UIColorFromHex(rgbValue: 0x4E8D7E, alpha: 1)
+        //title
+        score2Button1.setTitle("+", for: .normal)
+        //making the button clickable
+        score2Button1.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        //rounding corners
+        score2Button1.layer.cornerRadius = 15
+        score2Button1.layer.masksToBounds = true
+        //tag is used for differentiating buttons
+        score2Button1.tag = 2
+
     }
         
 //function for when first button is tapped
     @objc func buttonAction(sender: UIButton!) {
             if sender.tag == 1 {
-                print("button pressed")
+                score1Button1.backgroundColor = UIColorFromHex(rgbValue: 0xDCECB0)
+                score1Button2.backgroundColor = UIColorFromHex(rgbValue: 0xDCECB0)
+            points = points + 5
+            titleLabel.text = "\(points)\n Points"
+                
+            }
+            if sender.tag == 2 {
+                score2Button1.backgroundColor = UIColorFromHex(rgbValue: 0xDCECB0)
+                score2Button2.backgroundColor = UIColorFromHex(rgbValue: 0xDCECB0)
+            points = points + 10
+            titleLabel.text = "\(points)\n Points"
             }
         }
     
