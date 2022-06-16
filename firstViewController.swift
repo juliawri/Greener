d3d1aca7080266be1b3d3bf88581ed237cbe1758
shabicha <shabichasureshkumar@gmail.com>
2022-06-16 10:10:50 -0400

    added draft scroll

---
+++ firstViewController.swift
@@ -0,0 +1,31 @@
+//
+//  firstViewController.swift
+//  Greener
+//
+//  Created by Shabicha Sureshkumar on 2022-06-16.
+//
+
+import UIKit
+
+class firstViewController: UIViewController {
+
+    @IBOutlet weak var randomLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        randomlabel.text = "hjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxvhjabghjsxv"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
