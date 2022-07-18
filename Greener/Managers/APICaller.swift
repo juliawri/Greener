00b8c47e4e5ab0431098c0fce4b3c45003c065c8
Julia Wright <jwrighthfx@gmail.com>
2022-07-18 11:08:41 -0300

    working on backend of search

---
+++ Greener/Managers/APICaller.swift
@@ -0,0 +1,32 @@
+//
+//  APICaller.swift
+//  Greener
+//
+//  Created by Julia Wright on 2022-07-17.
+//
+
+import Foundation
+
+
+// MARK: - Category
+    
+public func getTasks(completion: (Result<String, Error>) -> Void) {
+    CFHTTPMessageCreateRequest(with: URL(string: Constants.baseAPIURL + "/browse/categories?limit=2"), type: .GET)
+    { request in
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else{
+                completion(.failure(APIError.failedtoGetData))
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                print(json)
+            }
+            catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+}
